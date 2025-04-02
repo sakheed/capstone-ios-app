6757b8edb886a01ef7059fc17d4417c6ac4eb60a
Sakhee Desai <sakheed@udel.edu>
2025-04-02 17:50:41 -0400

    crearing and training ML classification model lessgo

--- CAPSTONE/CAPSTONEApp.swift
+++ CAPSTONE/CAPSTONEApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct CAPSTONEApp: App {
+    @StateObject private var detectionStore = DetectionDataStore()
     var body: some Scene {
         WindowGroup {
             ContentView()
+            .environmentObject(detectionStore) 
         }
     }
 }
